ce37e7072c50191e92f5fc1e74125b693587348c
Personal Profile <davina.leong.sy@gmail.com>
2019-06-02 20:17:35 +0800

    Defined schema to query many users

---
+++ app/GraphQL/Query/UsersQuery.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\GraphQL\Query;
+
+use GraphQL;
+use GrqphQL\Type\Definition\Type;
+use Rebing\GraphQL\Support\Query;
+use Rebing\GrahpQL\Support\SelectFields;
+use App\User;
+
+class UsersQuery extends Query {
+
+    protected $attributes = [
+        'name' => 'users'
+    ];
+
+    public function authorize(array $args = [])
+    {
+        return true;
+    }
+
+    public function type()
+    {
+        return Type::listOf(GrapghQL::type('Users'));
+    }
+
+    public function args()
+    {
+        return [
+            'ids' => [
+                'name' => 'ids',
+                'type' => Type::listOf(Type::int())
+            ]
+        ];
+    }
+
+    public function rules(array $args = [])
+    {
+        return [
+            'ids' => [
+                'array',
+            ],
+            'ids.*' => [
+                'numeric'
+            ]
+        ];
+    }
+
+    public function resolve($root, $args)
+    {
+        if (isset($args['ids'])) {
+            return User::find($args['ids']);
+        }
+
+        return User::all();
+    }
+
+}
