04c42a2c699c0282590397a73ac54c0656fbdf8b
Personal Profile <davina.leong.sy@gmail.com>
2019-06-05 22:31:35 +0800

    Create source model and factory

--- database/migrations/2019_06_05_140726_create_sources_table.php
+++ database/migrations/2019_06_05_140726_create_sources_table.php
@@ -16,7 +16,6 @@ class CreateSourcesTable extends Migration
         Schema::create('sources', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('table_code');
             $table->string('group');
             $table->timestamps();
         });
