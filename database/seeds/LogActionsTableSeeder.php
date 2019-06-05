83f50d3b0aed4c0caf0abec388e6b235ea2c9a57
Personal Profile <davina.leong.sy@gmail.com>
2019-06-05 13:46:59 +0800

    Fixed seeder for Log Actions table

--- database/seeds/LogActionsTableSeeder.php
+++ database/seeds/LogActionsTableSeeder.php
@@ -12,42 +12,43 @@ class LogActionsTableSeeder extends Seeder
      */
     public function run()
     {
+        $now = new \DateTime('now', new \DateTimeZone('Asia/Singapore'));
         DB::table('log_actions')->insert([
             [
                 'name' => 'Others',
                 'action_code' => 'O',
-                'created_at' => 'CURRENT_TIMESTAMP',
-                'updated_at' => 'CURRENT_TIMESTAMP'
+                'created_at' => $now->format('Y-m-d H:i:s'),
+                'updated_at' => $now->format('Y-m-d H:i:s')
             ], [
                 'name' => 'Create',
                 'action_code' => 'C',
-                'created_at' => 'CURRENT_TIMESTAMP',
-                'updated_at' => 'CURRENT_TIMESTAMP'
+                'created_at' => $now->format('Y-m-d H:i:s'),
+                'updated_at' => $now->format('Y-m-d H:i:s')
             ], [
                 'name' => 'Retrieve',
                 'action_code' => 'R',
-                'created_at' => 'CURRENT_TIMESTAMP',
-                'updated_at' => 'CURRENT_TIMESTAMP'
+                'created_at' => $now->format('Y-m-d H:i:s'),
+                'updated_at' => $now->format('Y-m-d H:i:s')
             ], [
                 'name' => 'Update',
                 'action_code' => 'U',
-                'created_at' => 'CURRENT_TIMESTAMP',
-                'updated_at' => 'CURRENT_TIMESTAMP'
+                'created_at' => $now->format('Y-m-d H:i:s'),
+                'updated_at' => $now->format('Y-m-d H:i:s')
             ], [
                 'name' => 'Delete',
                 'action_code' => 'D',
-                'created_at' => 'CURRENT_TIMESTAMP',
-                'updated_at' => 'CURRENT_TIMESTAMP'
+                'created_at' => $now->format('Y-m-d H:i:s'),
+                'updated_at' => $now->format('Y-m-d H:i:s')
             ], [
                 'name' => 'Auth',
                 'action_code' => 'A',
-                'created_at' => 'CURRENT_TIMESTAMP',
-                'updated_at' => 'CURRENT_TIMESTAMP'
+                'created_at' => $now->format('Y-m-d H:i:s'),
+                'updated_at' => $now->format('Y-m-d H:i:s')
             ], [
                 'name' => 'Transaction',
                 'action_code' => 'T',
-                'created_at' => 'CURRENT_TIMESTAMP',
-                'updated_at' => 'CURRENT_TIMESTAMP'
+                'created_at' => $now->format('Y-m-d H:i:s'),
+                'updated_at' => $now->format('Y-m-d H:i:s')
             ]
          ]);
     }
