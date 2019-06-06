b2a72b3f182969fac60a7b05a6f98fbccb44ff51
Personal Profile <davina.leong.sy@gmail.com>
2019-06-06 20:34:46 +0800

    Setup tag routes

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class TagController extends Controller
 {
-    //
+    /**
+     * Load the Tag tables page.
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function index()
+    {
+        return view('tag.index');
+    }
 }
