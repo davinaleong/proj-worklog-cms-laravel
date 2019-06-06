be6876bfdc51da7f3658f68596d78e265345d8ee
Personal Profile <davina.leong.sy@gmail.com>
2019-06-06 10:14:42 +0800

    Added seeder for Years table

---
+++ database/seeds/YearsTableSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class YearsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $now = new \DateTime('now', new \DateTimeZone('Asia/Singapore'));
+        DB::table('years')->insert([
+            [
+                'year_title' => '2015',
+                'created_at' => $now->format('Y-m-d H:i:s'),
+                'updated_at' => $now->format('Y-m-d H:i:s')
+            ], [
+                'year_title' => '2016',
+                'created_at' => $now->format('Y-m-d H:i:s'),
+                'updated_at' => $now->format('Y-m-d H:i:s')
+            ], [
+                'year_title' => '2017',
+                'created_at' => $now->format('Y-m-d H:i:s'),
+                'updated_at' => $now->format('Y-m-d H:i:s')
+            ], [
+                'year_title' => '2018',
+                'created_at' => $now->format('Y-m-d H:i:s'),
+                'updated_at' => $now->format('Y-m-d H:i:s')
+            ], [
+                'year_title' => '2019',
+                'created_at' => $now->format('Y-m-d H:i:s'),
+                'updated_at' => $now->format('Y-m-d H:i:s')
+            ]
+        ]);
+    }
+}
