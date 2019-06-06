4ad4d92fbeaef1dd9b4d7a56098ec37a30898c03
Personal Profile <davina.leong.sy@gmail.com>
2019-06-06 22:09:43 +0800

    Setup configs for enums and date formats

---
+++ config/date_formats.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    'default_timezone' => 'Asia/Singapore',
+
+    'default_format' => 'U',
+
+    'other_formats' => [
+        'mysql' => 'Y-m-d H:i:s',
+        'display' => 'd-m-Y H:i:s',
+        'datetime' => 'd M Y, h:i a',
+        'date_only' => 'd M Y',
+        'time_only' => 'h:i a',
+        'time_24hr' => 'H:i'
+    ]
+
+];
