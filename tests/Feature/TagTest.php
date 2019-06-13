d113ef2b37bc9d236a180df13a19fccce361c094
Personal Profile <davina.leong.sy@gmail.com>
2019-06-13 21:29:46 +0800

    Wrote tests for other table forms

--- tests/Feature/TagTest.php
+++ tests/Feature/TagTest.php
@@ -93,6 +93,64 @@ class TagTest extends TestCase
     /**
      * @test
      */
+    public function user_successfully_submits_companies_form()
+    {
+        $this->withoutExceptionHandling();
+
+        // Sign in
+        $this->actingAs(factory(User::class)->create());
+
+        // Declare attributes
+        $attributes = [
+            'count' => 2,
+            'model' => 'Company',
+            'company[1][title]' => 'Company A',
+            'company[1][code]' => 'A',
+            'company[1][period_started_on]' => '01-06-2019 08:00:00',
+            'company[1][period_ended_on]' => null,
+            'company[2][title]' => 'Company B',
+            'company[2][code]' => 'B',
+            'company[2][period_started_on]' => '01-06-2019 08:00:00',
+            'company[2][period_ended_on]' => null
+        ];
+
+        // Attempt to submit form
+        $response = $this->put(route('tag.update'), $attributes);
+
+        // Assert for REDIRECT response to indicate successful form submission
+        $response->assertStatus(302);
+    }
+
+    /**
+     * @test
+     */
+    public function user_successfully_submits_projects_form()
+    {
+        $this->withoutExceptionHandling();
+
+        // Sign in
+        $this->actingAs(factory(User::class)->create());
+
+        // Declare attributes
+        $attributes = [
+            'count' => 2,
+            'model' => 'Project',
+            'project[1][title]' => 'Project A',
+            'project[1][code]' => 'A',
+            'project[2][title]' => 'Project B',
+            'project[2][code]' => 'B'
+        ];
+
+        // Attempt to submit form
+        $response = $this->put(route('tag.update'), $attributes);
+
+        // Assert for REDIRECT response to indicate successful form submission
+        $response->assertStatus(302);
+    }
+
+    /**
+     * @test
+     */
     public function user_successfully_submits_year_form()
     {
         $this->withoutExceptionHandling();
@@ -114,6 +172,58 @@ class TagTest extends TestCase
             ->put(route('tag.update'), $attributes);
 
         // Assert a REDIRECT response to indicate successful form submission
+        $response->assertStatus(302);
+    }
+
+    /**
+     * @test
+     */
+    public function user_successfully_submits_daytypes_form()
+    {
+        $this->withoutExceptionHandling();
+
+        // Sign in
+        $this->actingAs(factory(User::class)->create());
+
+        // Declare attributes
+        $attributes = [
+            'count' => 1,
+            'model' => 'DayType',
+            'daytype[1][title]' => 'Flag Day',
+            'daytype[1][code]' => 'F',
+            'daytype[1][icon_fa]' => 'fas fa-fw fa-flag',
+            'daytype[1][color_hex]' => '#888888'
+        ];
+
+        // Attempt to submit form
+        $response = $this->put(route('tag.update'), $attributes);
+
+        // Assert for REDIRECT response to indicate successful form submission
+        $response->assertStatus(302);
+    }
+
+**
+* @test
+*/
+    public function user_successfully_deletes_a_year_record()
+    {
+        $this->withoutExceptionHandling();
+
+        // Sign in
+        $this->actingAs(factory(User::class)->create());
+
+        // Create some Year records
+        factory(Year::class)->create();
+
+        // Declare form attributes
+        $attributes = [
+            'year1-remove' => 'remove'
+        ];
+
+        // Attempt to submit form
+        $response = $this->put(route('tag.update'), $attributes);
+
+        // Assert for REDIRECT response to indicate successful form submission
         $response->assertStatus(302);
     }
 
