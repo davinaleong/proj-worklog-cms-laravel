01ce0bf666d8e91d751012982e4bb1e71cc3ff47
Personal Profile <davina.leong.sy@gmail.com>
2019-06-13 21:13:25 +0800

    Fixed test for year table form

--- tests/Feature/TagTest.php
+++ tests/Feature/TagTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use App\Company;
 use App\Project;
 use App\Year;
+use Illuminate\Support\Facades\Session;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -97,205 +98,23 @@ class TagTest extends TestCase
         $this->withoutExceptionHandling();
 
         // Sign in
-        $this->actingAs(factory(User::class)->create());
+        $user = factory(User::class)->create();
 
         // Declare form attributes
         $attributes = [
+            'count' => 2,
+            'model' => 'Year',
             'year[1][title]' => '2020',
             'year[2][title]' => '2015'
         ];
 
         // Attempt to submit form
-        $response = $this->put(route('tag.update'), $attributes);
+        $response = $this->actingAs($user)
+            ->withSession(['foo' => 'bar'])
+            ->put(route('tag.update'), $attributes);
 
         // Assert a REDIRECT response to indicate successful form submission
         $response->assertStatus(302);
     }
 
-    /**
-     * @test
-     */
-    public function user_successfully_deletes_a_year_record()
-    {
-        $this->withoutExceptionHandling();
-
-        // Sign in
-        $this->actingAs(factory(User::class)->create());
-
-        // Create some Year records
-        factory(Year::class)->create();
-
-        // Declare form attributes
-        $attributes = [
-            'year1-remove' => 'remove'
-        ];
-
-        // Attempt to submit form
-        $response = $this->put(route('tag.update'), $attributes);
-
-        // Assert for REDIRECT response to indicate successful form submission
-        $response->assertStatus(302);
-    }
-
-    /**
-     * @test
-     */
-    public function user_successfully_submits_companies_form()
-    {
-        $this->withoutExceptionHandling();
-
-        // Sign in
-        $this->actingAs(factory(User::class)->create());
-
-        // Declare attributes
-        $attributes = [
-            'new0-title_company' => 'Company A',
-            'new0-code_company' => 'A',
-            'new0-period_started_on' => '01-06-2019 08:00:00',
-            'new0-period_ended_on' => null,
-            'new1-title_company' => 'Company B',
-            'new1-code_company' => 'B',
-            'new1-period_started_on' => '01-06-2019 08:00:00',
-            'new1-period_ended_on' => null
-        ];
-
-        // Attempt to submit form
-        $response = $this->put(route('tag.update'), $attributes);
-
-        // Assert for REDIRECT response to indicate successful form submission
-        $response->assertStatus(302);
-    }
-
-    /**
-     * @test
-     */
-    public function user_successfully_deletes_a_company_record()
-    {
-        $this->withoutExceptionHandling();
-
-        // Sign in
-        $this->actingAs(factory(User::class)->create());
-
-        // Create some Company records
-        factory(Company::class)->create();
-
-        // Declare attributes
-        $attributes = [
-            'company1-remove' => 'remove'
-        ];
-
-        // Attempt to submit form
-        $response = $this->put(route('tag.update'), $attributes);
-
-        // Assert for REDIRECT response to indicate successful form submission
-        $response->assertStatus(302);
-    }
-
-    /**
-     * @test
-     */
-    public function user_successfully_submits_projects_form()
-    {
-        $this->withoutExceptionHandling();
-
-        // Sign in
-        $this->actingAs(factory(User::class)->create());
-
-        // Declare attributes
-        $attributes = [
-            'new0-title_project' => 'Project A',
-            'new0-code_project' => 'A',
-            'new1-title_project' => 'Project B',
-            'new1-code_project' => 'B'
-        ];
-
-        // Attempt to submit form
-        $response = $this->put(route('tag.update'), $attributes);
-
-        // Assert for REDIRECT response to indicate successful form submission
-        $response->assertStatus(302);
-    }
-
-    /**
-     * @test
-     */
-    public function user_successfully_deletes_a_project()
-    {
-        $this->withoutExceptionHandling();
-
-        // Sign in
-        $this->actingAs(factory(User::class)->create());
-
-        // Create a Project record
-        factory(Project::class)->create([
-            'title_project' => 'Fun Project',
-            'code_project' => 'FUN'
-        ]);
-
-        // Declare attributes
-        $attributes = [
-            'project1-remove' => 'remove'
-        ];
-
-        // Attempt to submit form
-        $response = $this->get(route('tag.update'), $attributes);
-
-        // Assert for REDIRECT response to indicate successful form submission
-        $response->assertStatus(302);
-    }
-
-    /**
-     * @test
-     */
-    public function user_successfully_submits_daytypes_form()
-    {
-        $this->withoutExceptionHandling();
-
-        // Sign in
-        $this->actingAs(factory(User::class)->create());
-
-        // Declare attributes
-        $attributes = [
-            'new0-title_type' => 'Flag Day',
-            'new0-code_type' => 'F',
-            'new0-icon_fa' => 'fas fa-fw fa-flag',
-            'new0-color-hex' => '#888888'
-        ];
-
-        // Attempt to submit form
-        $response = $this->get(route('tag.update'), $attributes);
-
-        // Assert for REDIRECT response to indicate successful form submission
-        $response->assertStatus(302);
-    }
-
-    /**
-     * @test
-     */
-    public function user_successfully_deletes_a_daytype()
-    {
-        $this->withoutExceptionHandling();
-
-        // Sign in
-        $this->actingAs(factory(User::class)->create());
-
-        // Create a Project record
-        factory(DayType::class)->create([
-            'title_type' => 'Flag Day',
-            'code_type' => 'F',
-            'icon_fa' => 'fas fa-fw fa-flag',
-            'color_hex' => '#888888'
-        ]);
-
-        // Declare attributes
-        $attributes = [
-            'daytype1-remove' => 'remove'
-        ];
-
-        // Attempt to submit form
-        $response = $this->get(route('tag.update'), $attributes);
-
-        // Assert for REDIRECT response to indicate successful form submission
-        $response->assertStatus(302);
-    }
 }
