06167adc08db2e75e1e20b0aff648752ff3b9efa
Personal Profile <davina.leong.sy@gmail.com>
2019-06-15 16:05:34 +0800

    Create migrations for work log tables

---
+++ database/migrations/2019_06_15_155715_create_log_entries_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLogEntriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('log_entries', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('log_id', false, true);
+            $table->integer('creator_id', false, true);
+            $table->string('title_entry')->nullable();
+            $table->string('code_type')->nullable();
+            $table->date('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('log_entries');
+    }
+}
