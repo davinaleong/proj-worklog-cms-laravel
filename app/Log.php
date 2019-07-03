684ea89411de8d113efdafbadf480cb5afc31952
Personal Profile <davina.leong.sy@gmail.com>
2019-07-03 09:34:30 +0800

    Created relationships between work log models

--- app/Log.php
+++ app/Log.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Log extends Model
 {
-    //
+    /**
+     * Returns entries of the Log.
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function entries() {
+        return $this->hasMany('App\LogEntry');
+    }
 }
--- app/LogEntry.php
+++ app/LogEntry.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class LogEntry extends Model
 {
-    //
+    /**
+     * Returns the items of a Log Entry.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function items() {
+        return $this->hasMany('App\EntryItem');
+    }
 }
