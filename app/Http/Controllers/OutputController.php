6cdbd93eccaafcbe9259ad9cf7e8a5b94175de10
Personal Profile <davina.leong.sy@gmail.com>
2019-07-06 11:24:10 +0800

    Output details of current year and company

--- app/Http/Controllers/OutputController.php
+++ app/Http/Controllers/OutputController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\CodeModel;
+use App\Company;
+use App\Year;
 use Illuminate\Http\Request;
 
 class OutputController extends Controller
@@ -13,8 +15,13 @@ class OutputController extends Controller
     public function index()
     {
         $codeModel = new CodeModel();
+
+        $now = new \DateTime('now', new \DateTimeZone(config('app.timezone')));
+
         return view('output.index', [
-            'codeModel' => $codeModel
+            'codeModel' => $codeModel,
+            'year' => Year::where('title_year', '=', $now->format('Y'))->firstOrFail(),
+            'company' => Company::where('code_company', '=', 'TPA')->firstOrFail()
         ]);
     }
 }
--- resources/views/output/index.blade.php
+++ resources/views/output/index.blade.php
@@ -1,12 +1,40 @@
 <html>
-    <body>
-        <h1>Model Codes</h1>
-        
-        <pre>@php var_dump($codeModel->dayTypes()) @endphp</pre>
-        <pre>{{ $codeModel->dayTypes(',') }}</pre>
-        <pre>@php var_dump($codeModel->projects()) @endphp</pre>
-        <pre>{{ $codeModel->projects(',') }}</pre>
-        <pre>@php var_dump($codeModel->dbLogActions()) @endphp</pre>
-        <pre>{{ $codeModel->dbLogActions(',') }}</pre>
-    </body>
+<head>
+    <style>
+        table, th, td {
+            border: thin solid #000
+        }
+    </style>
+</head>
+<body>
+    <h1>Output</h1>
+
+    <h2>Current</h2>
+    <table>
+        <thead>
+        <tr>
+            <th>ID</th>
+            <th>Title</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>{{ $year->id }}</td>
+            <td>{{ $year->title_year }}</td>
+        </tr>
+        <tr>
+            <td>{{ $company->id }}</td>
+            <td>{{ $company->title_company }} ({{ $company->code_company }})</td>
+        </tr>
+        </tbody>
+    </table>
+
+    <h2>Model Codes</h2>
+    <pre>@php var_dump($codeModel->dayTypes()) @endphp</pre>
+    <pre>{{ $codeModel->dayTypes(',') }}</pre>
+    <pre>@php var_dump($codeModel->projects()) @endphp</pre>
+    <pre>{{ $codeModel->projects(',') }}</pre>
+    <pre>@php var_dump($codeModel->dbLogActions()) @endphp</pre>
+    <pre>{{ $codeModel->dbLogActions(',') }}</pre>
+</body>
 </html>
